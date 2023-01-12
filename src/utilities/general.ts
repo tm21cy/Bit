91df61c2e7880dc6997c90f12ca5850e8741afa6
tm21cy <tm21cy@brocku.ca>
2023-01-12 14:44:35 -0500

    refactor: cleanup utilities class
    Code cleanup and refactor of general purpose utils class.

--- src/utilities/general.ts
+++ src/utilities/general.ts
@@ -3,34 +3,39 @@
  * @class
  */
 class util {
-	/**
-	 * Decodes a base64 string.
-	 * @param {string} base64String The base64 string to decode.
-	 * @returns {string} The decoded string.
-	 */
-	public static decodeBase64(base64String: string): string {
-		return Buffer.from(base64String, "base64").toString("ascii")
-	}
+  /**
+   * Decodes a base64 string.
+   * @param {string} base64String The base64 string to decode.
+   * @returns {string} The decoded string.
+   */
+  public static decodeBase64(base64String: string): string {
+    return Buffer.from(base64String, "base64").toString("ascii");
+  }
 
-	/**
-	 * Verifies the authenticity of a developer's usage of a command.
-	 * @param id The ID of the user using the command.
-	 * @returns 
-	 */
-	public static verifyDev(id: string): boolean {
-		return (id == process.env.DEV_ID_1 || id == process.env.DEV_ID_1)
-	}
+  /**
+   * Verifies the authenticity of a developer's usage of a command.
+   * @param id The ID of the user using the command.
+   * @returns
+   */
+  public static verifyDev(id: string): boolean {
+    return id == process.env.DEV_ID_1 || id == process.env.DEV_ID_1;
+  }
 
-	public static formatTimestamp(option: "date" | "dateAndRelative" | "relative", timestamp: number): string {
-		switch (option) {
-			case "date":
-				return `<t:${Math.round(timestamp / 1000)}>`
-			case "dateAndRelative":
-				return `<t:${Math.round(timestamp / 1000)}> (<t:${Math.round(timestamp / 1000)}:R>)`
-			case "relative":
-				return `<t:${Math.round(timestamp / 1000)}:R>`
-		}
-	}
+  public static formatTimestamp(
+    option: "date" | "dateAndRelative" | "relative",
+    timestamp: number
+  ): string {
+    switch (option) {
+      case "date":
+        return `<t:${Math.round(timestamp / 1000)}>`;
+      case "dateAndRelative":
+        return `<t:${Math.round(timestamp / 1000)}> (<t:${Math.round(
+          timestamp / 1000
+        )}:R>)`;
+      case "relative":
+        return `<t:${Math.round(timestamp / 1000)}:R>`;
+    }
+  }
 }
 
-export default util
+export default util;
