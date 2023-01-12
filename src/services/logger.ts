016248e53e580e8db5115180dca9677349a924e8
tm21cy <tm21cy@brocku.ca>
2023-01-12 14:43:09 -0500

    refactor: code cleanup - services
    Code quality improvements for service handlers.

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,14 +1,14 @@
-import * as dotenv from "dotenv"
-import pino from "pino"
+import * as dotenv from "dotenv";
+import pino from "pino";
 const log = pino({
-	transport: {
-		target: "pino-pretty",
-		options: {
-			colorize: true,
-			ignore: "pid,hostname",
-		}
-	}
-})
-dotenv.config()
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+      ignore: "pid,hostname",
+    },
+  },
+});
+dotenv.config();
 
-export { log }
+export { log };
