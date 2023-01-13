a27c5061274951295494b34f79beffef8135a820
net-tech <hello@nettech.link>
2023-01-13 03:38:43 +0100

    refactor: rename util to Util and add generateTimestamp
    Signed-off-by: net-tech <hello@nettech.link>

--- src/commands/dev.ts
+++ src/commands/dev.ts
@@ -1,87 +1,86 @@
 import {
-  ChatInputCommandInteraction,
-  EmbedBuilder,
-  SlashCommandBuilder,
-  TextChannel,
+	ChatInputCommandInteraction,
+	EmbedBuilder,
+	PermissionFlagsBits,
+	SlashCommandBuilder,
+	TextChannel,
 } from "discord.js";
 import { client } from "..";
 import { log } from "../services/logger";
-import util from "../utilities/general";
+import Util from "../utilities/general";
 import { exec } from "child_process";
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("dev")
-    .setDescription("Execute developer functions.")
-    .addSubcommand((subcommand) =>
-      subcommand.setName("restart").setDescription("Restart the bot process")
-    )
-    .addSubcommand((subcommand) =>
-      subcommand.setName("shutdown").setDescription("Shutdown the bot process.")
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName("debug")
-        .setDescription("See debugging related statistics.")
-    ),
-  async execute(interaction: ChatInputCommandInteraction) {
-    const opt = interaction.options.getSubcommand();
-    if (opt == "restart") {
-      const date = new Date();
-      log.debug(
-        `Restart command issued by user ID ${
-          interaction.user.id
-        } at ${date.toTimeString()}.`
-      );
-      const embed = new EmbedBuilder()
-        .setTitle("Restarting Container")
-        .setDescription(
-          `A manual restart has been issued by **${interaction.user.tag}** (\`${
-            interaction.user.id
-          }\`) at ${util.formatTimestamp("dateAndRelative", date.getTime())}.`
-        )
-        .setColor("Grey");
-      const channel = (await interaction.guild?.channels.fetch(
-        "959911773480316938"
-      )) as TextChannel;
-      channel.send({ embeds: [embed] });
-      await interaction.reply({ content: "✅" });
-      exec("pm2 restart bit", function (error: any, stdout: any, stderr: any) {
-        interaction.reply({
-          content: `stdout: ${stdout}\n\nstderr: ${stderr}`,
-        });
-        if (error !== null) {
-          interaction.channel?.send({ content: `exec error: ${error}` });
-        }
-      });
-    } else if (opt == "shutdown") {
-	  const date = new Date();
-	  log.debug(
-		`Shutdown command issued by user ID ${
-		  interaction.user.id
-		} at ${date.toTimeString()}.`
-	  );
-	  const embed = new EmbedBuilder()
-		.setTitle("Shutting Down Container")
-		.setDescription(
-		  `A manual shutdown has been issued by **${interaction.user.tag}** (\`${
-			interaction.user.id
-		  }\`) at ${util.formatTimestamp("dateAndRelative", date.getTime())}.`
+	data: new SlashCommandBuilder()
+		.setName("dev")
+		.setDescription("Execute developer functions.")
+		.setDMPermission(false)
+		.setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+		.addSubcommand((subcommand) =>
+			subcommand.setName("restart").setDescription("Restart the bot process")
 		)
-		.setColor("Grey");
-	  const channel = (await interaction.guild?.channels.fetch(
-		"959911773480316938"
-	  )) as TextChannel;
-	  channel.send({ embeds: [embed] });
-	  await interaction.reply({ content: "✅" });
-	  exec("pm2 stop bit", function (error: any, stdout: any, stderr: any) {
-		interaction.reply({
-		  content: `stdout: ${stdout}\n\nstderr: ${stderr}`,
-		});
-		if (error !== null) {
-		  interaction.channel?.send({ content: `exec error: ${error}` });
+		.addSubcommand((subcommand) =>
+			subcommand.setName("shutdown").setDescription("Shutdown the bot process.")
+		)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("debug")
+				.setDescription("See debugging related statistics.")
+		),
+	async execute(interaction: ChatInputCommandInteraction) {
+		const opt = interaction.options.getSubcommand();
+		if (opt == "restart") {
+			const date = new Date();
+			log.debug(
+				`Restart command issued by user ID ${interaction.user.id
+				} at ${date.toTimeString()}.`
+			);
+			const embed = new EmbedBuilder()
+				.setTitle("Restarting Container")
+				.setDescription(
+					`A manual restart has been issued by **${interaction.user.tag}** (\`${interaction.user.id
+					}\`) at ${Util.formatTimestamp("dateAndRelative", date.getTime())}.`
+				)
+				.setColor("Grey");
+			const channel = (await interaction.guild?.channels.fetch(
+				"959911773480316938"
+			)) as TextChannel;
+			channel.send({ embeds: [embed] });
+			await interaction.reply({ content: "✅" });
+			exec("pm2 restart bit", function (error: any, stdout: any, stderr: any) {
+				interaction.reply({
+					content: `stdout: ${stdout}\n\nstderr: ${stderr}`,
+				});
+				if (error !== null) {
+					interaction.channel?.send({ content: `exec error: ${error}` });
+				}
+			});
+		} else if (opt == "shutdown") {
+			const date = new Date();
+			log.debug(
+				`Shutdown command issued by user ID ${interaction.user.id
+				} at ${date.toTimeString()}.`
+			);
+			const embed = new EmbedBuilder()
+				.setTitle("Shutting Down Container")
+				.setDescription(
+					`A manual shutdown has been issued by **${interaction.user.tag}** (\`${interaction.user.id
+					}\`) at ${Util.formatTimestamp("dateAndRelative", date.getTime())}.`
+				)
+				.setColor("Grey");
+			const channel = (await interaction.guild?.channels.fetch(
+				"959911773480316938"
+			)) as TextChannel;
+			channel.send({ embeds: [embed] });
+			await interaction.reply({ content: "✅" });
+			exec("pm2 stop bit", function (error: any, stdout: any, stderr: any) {
+				interaction.reply({
+					content: `stdout: ${stdout}\n\nstderr: ${stderr}`,
+				});
+				if (error !== null) {
+					interaction.channel?.send({ content: `exec error: ${error}` });
+				}
+			});
 		}
-	  });
-    }
-  },
+	},
 };
--- src/utilities/general.ts
+++ src/utilities/general.ts
@@ -1,41 +1,53 @@
+import { TimestampStylesString } from "discord.js";
+
 /**
  * Provides general utility functions which cannot be categorized.
  * @class
  */
-class util {
-  /**
-   * Decodes a base64 string.
-   * @param {string} base64String The base64 string to decode.
-   * @returns {string} The decoded string.
-   */
-  public static decodeBase64(base64String: string): string {
-    return Buffer.from(base64String, "base64").toString("ascii");
-  }
+class Util {
+	/**
+	 * Decodes a base64 string.
+	 * @param {string} base64String The base64 string to decode.
+	 * @returns {string} The decoded string.
+	 */
+	public static decodeBase64(base64String: string): string {
+		return Buffer.from(base64String, "base64").toString("ascii");
+	}
 
-  /**
-   * Verifies the authenticity of a developer's usage of a command.
-   * @param id The ID of the user using the command.
-   * @returns
-   */
-  public static verifyDev(id: string): boolean {
-    return id == process.env.DEV_ID_1 || id == process.env.DEV_ID_1;
-  }
+	/**
+	 * Verifies the authenticity of a developer's usage of a command.
+	 * @param id The ID of the user using the command.
+	 * @returns
+	 */
+	public static verifyDev(id: string): boolean {
+		return id == process.env.DEV_ID_1 || id == process.env.DEV_ID_1;
+	}
 
-  public static formatTimestamp(
-    option: "date" | "dateAndRelative" | "relative",
-    timestamp: number
-  ): string {
-    switch (option) {
-      case "date":
-        return `<t:${Math.round(timestamp / 1000)}>`;
-      case "dateAndRelative":
-        return `<t:${Math.round(timestamp / 1000)}> (<t:${Math.round(
-          timestamp / 1000
-        )}:R>)`;
-      case "relative":
-        return `<t:${Math.round(timestamp / 1000)}:R>`;
-    }
-  }
+	public static formatTimestamp(
+		option: "date" | "dateAndRelative" | "relative",
+		timestamp: number
+	): string {
+		switch (option) {
+			case "date":
+				return `<t:${Math.round(timestamp / 1000)}>`;
+			case "dateAndRelative":
+				return `<t:${Math.round(timestamp / 1000)}> (<t:${Math.round(
+					timestamp / 1000
+				)}:R>)`;
+			case "relative":
+				return `<t:${Math.round(timestamp / 1000)}:R>`;
+		}
+	}
+
+	public static generateTimestamp(style: TimestampStylesString, timestamp: number | Date, addRelative = false) {
+		if (typeof timestamp === typeof Date) {
+			timestamp = Math.round((timestamp as Date).getTime() / 1000)
+		} else {
+			timestamp = Math.round((timestamp as number) / 1000)
+		}
+
+		return `<t:${timestamp}:${style}>${addRelative ? ` (<t:${timestamp}:R>)` : ""}`
+	}
 }
 
-export default util;
+export default Util;
