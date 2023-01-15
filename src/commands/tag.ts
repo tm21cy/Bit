98824f90bd660499feb7b13b36f0aaf3801c3be4
net-tech <hello@nettech.link>
2023-01-15 05:04:11 +0100

    chore: command in development disclaimer
    Signed-off-by: net-tech <hello@nettech.link>

--- src/commands/tag.ts
+++ src/commands/tag.ts
@@ -3,8 +3,21 @@ import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js"
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("tag")
-		.setDescription("Use a tag."),
+		.setDescription("Use a tag.")
+		.addStringOption((option) =>
+			option
+				.setName("name")
+				.setDescription("The name of the tag.")
+				.setRequired(true)
+				// .setAutocomplete(true)
+		),
 	async execute(interaction: ChatInputCommandInteraction) {
 
+		//const subcommand = interaction.options.getSubcommand();
+
+		await interaction.reply({
+			content: "This command is not yet implemented.",
+			ephemeral: true,
+		})
 	}
 }
