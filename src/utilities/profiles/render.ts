5ba669a8f752db417ff0c5bb6a2149249b7e174a
tm21cy <tm21cy@brocku.ca>
2023-01-19 23:21:26 -0500

    refactor: improved rendering
    Improves upon the previous rendering system, as well as changing to allow v2 typings.

--- src/utilities/profiles/render.ts
+++ src/utilities/profiles/render.ts
@@ -1,10 +1,13 @@
-import { ProfileData } from "../../types/Interfaces";
-import { EmbedBuilder } from "discord.js";
+import { ColorResolvable, EmbedBuilder } from "discord.js";
 import Badges from "./badges";
 import badges from "./badges";
 import Query from "../../routes/Query";
+import badgesArray from "./badgesArray";
+import { includes } from "lodash";
+import Colors from "../../enums/colors";
+import { ProfileOutput } from "../../models/Profile";
 
-export default async function render(profile: ProfileData) {
+export default async function render(profile: ProfileOutput) {
   // Profile data destructure
   let display_name = profile.display_name;
   let badge_flags = profile.badge_flags;
@@ -19,6 +22,9 @@ export default async function render(profile: ProfileData) {
   // Preliminary data parsing
   let embedColor = "#5856d6";
   let badges = Badges.getBadgeEmojis(badge_flags);
+  let bret = Badges.getBadges(badge_flags);
+  let badgeNames = bret.names;
+  let badgeVals = bret.indices;
   let comments = await Query.comments.retrieveComments(user_id);
   let chunks: string[] = [];
 
@@ -31,7 +37,7 @@ export default async function render(profile: ProfileData) {
     );
   }
 
-  const embed = new EmbedBuilder().setColor("Blurple");
+  const embed = new EmbedBuilder();
   embed.setTitle(`${profile.display_name}`);
   embed.setThumbnail(display_picture);
   embed.addFields([
@@ -47,5 +53,28 @@ export default async function render(profile: ProfileData) {
   ]);
   embed.setDescription(`${chunks[0] == "" ? " " : chunks.join("\n")}`);
 
+  embed.setColor(getColor(badgeNames));
+
   return embed;
+}
+
+export function getColor(badges: string[]): ColorResolvable {
+  switch (true) {
+    case badges.includes("Root"):
+      return Colors.Indigo;
+    case badges.includes("SeniorMod"):
+      return Colors.Red;
+    case badges.includes("Mod"):
+      return Colors.Yellow;
+    case badges.includes("Proficient"):
+      return Colors.Purple;
+    case badges.includes("Fluent"):
+      return Colors.Teal;
+    case badges.includes("Active"):
+      return Colors.Active;
+    case badges.includes("Regular"):
+      return Colors.Green;
+    default:
+      return Colors.Blue;
+  }
 }
