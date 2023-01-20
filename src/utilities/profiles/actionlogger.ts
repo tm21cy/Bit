282ce4b6388d8f74822da5e1564fa77eaf9f802b
tm21cy <tm21cy@brocku.ca>
2023-01-19 23:19:49 -0500

    feat: add action logging
    Adds an action logger, with triggered actions sending an embed to our logging channel.

---
+++ src/utilities/profiles/actionlogger.ts
@@ -0,0 +1,30 @@
+import { Data } from "../../types/Interfaces";
+import { EmbedBuilder, TextChannel } from "discord.js";
+import Color from "../../enums/colors";
+import { client } from "../../index";
+
+export default function actionLog(
+  component:
+    | "Comments"
+    | "Helpers"
+    | "LikeUsers"
+    | "Notifications"
+    | "Profiles",
+  action: "GET" | "POST" | "PATCH" | "DELETE",
+  data: Data,
+  user: string
+) {
+  let loggingChannel = process.env.LOGGING;
+  if (!loggingChannel) throw new Error("No logging channel provided.");
+  let embed = new EmbedBuilder().setColor(Color.Red);
+  let userObj = client.users.resolve(user);
+  embed.setTitle(`New ${action} Action - ${component}`);
+  embed.setDescription(JSON.stringify(data, null, 4));
+  embed.addFields({
+    name: "Responsible User",
+    value: `${user} (${userObj?.tag})`,
+  });
+  return (client.channels.resolve(loggingChannel) as TextChannel).send({
+    embeds: [embed],
+  });
+}
