5d14ab6d841367370580d577d6197072767e1e67
tm21cy <tm21cy@brocku.ca>
2023-01-24 00:06:46 -0500

    feat: add notification subject and unread filtering
    Adds filtering to the Notification route for subjects and unread statuses.

--- src/routes/Notifications.ts
+++ src/routes/Notifications.ts
@@ -1,8 +1,4 @@
-import { QueryTypes } from "sequelize";
-import { db } from "../models/Sequelizes";
-import { DatabaseInsertError } from "../types/Errors";
 import { Get, Post, Status } from "../types/Interfaces";
-import { ReturnData } from "../types/Types";
 import * as NotificationService from "../services/api/NotificationService";
 
 /**
@@ -12,15 +8,21 @@ class Notifications {
   /**
    * POST operation to upload and sync a new Notification to the notifications table.
    * @param targetID The Discord ID snowflake of the destination user.
+   * @param subject A brief header describing the notification being posted.
    * @param text The text of the notification being synced.
    * @returns Promise containing a Post object, which contains the notification data, and a status code.
    */
-  async postNotification(targetID: string, text: string): Promise<Post> {
+  async postNotification(
+    targetID: string,
+    subject: string,
+    text: string
+  ): Promise<Post> {
     const timestamp = `${Math.round(new Date().getTime() / 1000)}`;
     let object = {
       target_id: targetID,
       timestamp,
       text,
+      subject,
     };
 
     let notification = await NotificationService.create(object);
@@ -42,6 +44,31 @@ class Notifications {
     ).slice(0, 5);
     return {
       data: retTuple,
+      status: Status.OK,
+    };
+  }
+
+  async retrieveUnread(targetID: string): Promise<Get> {
+    const retTuple = (
+      await NotificationService.getAll({
+        target_id: targetID,
+        marked_read: false,
+      })
+    ).slice(0, 5);
+    return {
+      data: retTuple,
+      status: Status.OK,
+    };
+  }
+
+  async markRead(id: number): Promise<void> {
+    await NotificationService.update(id, { marked_read: true });
+  }
+
+  async getNotification(id: number): Promise<Get> {
+    let notification = await NotificationService.getById(id);
+    return {
+      data: notification,
       status: Status.OK,
     };
   }
