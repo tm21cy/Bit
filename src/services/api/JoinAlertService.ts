791440c1bc81eefa2733540bad2c6f3a827e2bdd
tm21cy <tm21cy@brocku.ca>
2023-01-25 14:04:09 -0500

    feat: add support for join alerts

---
+++ src/services/api/JoinAlertService.ts
@@ -0,0 +1,29 @@
+import * as dal from "../../dal/JoinAlert";
+import {
+  JoinAlertInput as Input,
+  JoinAlertOutput as Output,
+} from "../../models/JoinAlert";
+import { JoinAlertDALFilters as DALFilters } from "../../dal/types/JoinAlertDALFilters";
+
+export const create = (payload: Input): Promise<Output> => {
+  return dal.create(payload);
+};
+
+export const update = (
+  id: number,
+  payload: Partial<Input>
+): Promise<Output> => {
+  return dal.update(id, payload);
+};
+
+export const getById = (id: number): Promise<Output> => {
+  return dal.getById(id);
+};
+
+export const deleteById = (id: number): Promise<boolean> => {
+  return dal.deleteById(id);
+};
+
+export const getAll = (filters?: DALFilters): Promise<Output[]> => {
+  return dal.getAll(filters);
+};
